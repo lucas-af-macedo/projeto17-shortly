8bac20ba6bf04e9a6a0ca1093dcc81303d614bd2
Lucas Macedo <lucasamacedo@hotmail.com>
2022-12-20 00:34:58 -0300

    feat: create sign up middlewares

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { signIn, signUp } from "../controllers/authController.js";
+import { existEmail, samePassword, signUpValidation } from "../middlewares/authMiddleware.js";
 
 const authRouter = express.Router();
 
-authRouter.post('/signup', signUp);
+authRouter.post('/signup', signUpValidation, samePassword, existEmail, signUp);
 authRouter.post('/signin', signIn);
 
 export default authRouter;
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { stripHtml } from 'string-strip-html';
 import router from './routers/index.js';
 
 export const cleanStringData = (string) =>
-    typeof string == "string" ? stripHtml(string).result.trim : string;
+    typeof string == "string" ? stripHtml(string).result.trim() : string;
 
 const app = express();
 app.use(cors());
