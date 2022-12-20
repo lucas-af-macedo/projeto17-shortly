a0e60f45f12c033786dc29071b66ffcc16d803fc
Lucas Macedo <lucasamacedo@hotmail.com>
2022-12-20 15:28:34 -0300

    feat: create get one url controller

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -16,8 +16,20 @@ export async function postUrl (req, res){
 }
 
 export async function getOneUrl (req, res){
+    const id = req.params.id;
+
     try{
-        res.sendStatus(501);
+        const query = await connection.query('SELECT * FROM urls WHERE "id"=$1',[id]);
+
+        if (query.rows.length){
+            const url = query.rows[0];
+            delete url.userId;
+            delete url.visitCount;
+            res.status(200).send(url);
+        } else{
+            res.sendStatus(404);
+        }
+
     } catch(err){
         res.sendStatus(500);
         console.log(err);
