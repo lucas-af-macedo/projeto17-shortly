be901df08caa3f404db5da002a74fd6d852edd00
Lucas Macedo <lucasamacedo@hotmail.com>
2022-12-20 14:20:28 -0300

    feat: create sign in controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,19 +1,17 @@
 import connection from "../database/database.js";
 import dayjs from "dayjs";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 export async function signUp (req, res){
     const now = dayjs().format("YYYY-MM-DD HH:mm");
-    const user = {
-        ...res.locals.user,
-        signupdate: now
-    }
+    const user = res.locals.user;
 
     const passwordHash = bcrypt.hashSync(user.password, 10);
 
     try{
 
-        await connection.query('INSERT INTO users (name, email, password, "signUpDate") VALUES ($1, $2, $3, $4)', [user.name, user.email, passwordHash, user.signupdate])
+        await connection.query('INSERT INTO users (name, email, password, "signUpDate") VALUES ($1, $2, $3, $4)', [user.name, user.email, passwordHash, now])
         res.sendStatus(201);
     } catch(err){
         res.sendStatus(500);
@@ -22,8 +20,19 @@ export async function signUp (req, res){
 }
 
 export async function signIn (req, res){
+    const user = res.locals.user;
+    const token = uuid();
+    const now = dayjs().format("YYYY-MM-DD HH:mm");
+
     try{
-        res.sendStatus(501);
+        const oldSession = await connection.query('SELECT * FROM sessions WHERE "userId"=$1',[user.id]);
+        if(oldSession.rows.length){
+            await connection.query('DELETE FROM sessions WHERE "userId"=$1',[user.id]);
+        }
+        
+        await connection.query('INSERT INTO sessions ("userId", token, "lastSignIn") VALUES ($1, $2, $3)',[user.id, token, now]);
+
+        res.status(200).send({token});
     } catch(err){
         res.sendStatus(500);
         console.log(err);
--- src/middlewares/signInMiddleware.js
+++ src/middlewares/signInMiddleware.js
@@ -4,13 +4,13 @@ import { cleanStringData } from "../server.js";
 import bcrypt from 'bcrypt';
 
 export async function signInEmailValidation (req, res, next){
-    const user = res.locals.user;
+    const signIn = res.locals.signIn;
 
     try{
-        const email = await connection.query('SELECT * FROM users WHERE email = $1',[user.email]);
+        const email = await connection.query('SELECT * FROM users WHERE email = $1',[signIn.email]);
         
         if (email.rows.length){
-            res.locals.password = email.rows[0].password;
+            res.locals.user = email.rows[0];
             next();
         } else {
             res.status(401).send({message: 'Usuário e/ou senha inválidos!'})
@@ -22,11 +22,11 @@ export async function signInEmailValidation (req, res, next){
 }
 
 export async function passwordValidation (req, res, next){
-    const user = res.locals.user;
-    const password = res.locals.password;
+    const signIn = res.locals.signIn;
+    const password = res.locals.user.password;
 
     try{
-        if(bcrypt.compareSync(user.password, password)){
+        if(bcrypt.compareSync(signIn.password, password)){
             next();
         }else{
             res.status(401).send({message:'Usuário e/ou senha inválidos!'})
@@ -39,20 +39,20 @@ export async function passwordValidation (req, res, next){
 
 
 export async function signInValidation (req, res, next){
-    const user = {};
+    const signIn = {};
 
     Object.keys(req.body).forEach(
-        (key) => user[key] = cleanStringData(req.body[key])
+        (key) => signIn[key] = cleanStringData(req.body[key])
     );
 
-    const validation = signInSchema.validate(user, { abortEarly: false });
+    const validation = signInSchema.validate(signIn, { abortEarly: false });
     
     if (validation.error) {
         const errors = validation.error.details.map((detail) => detail.message);
         res.status(422).send(errors)
         return;
     } else{
-        res.locals.user = user;
+        res.locals.signIn = signIn;
     }
     next();
 }
