fdcc259e161b82fcee2dafb8c10b9a496e0727e2
Lucas Macedo <lucasamacedo@hotmail.com>
2022-12-20 13:53:21 -0300

    feat: create sign up controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,20 @@
 import connection from "../database/database.js";
+import dayjs from "dayjs";
+import bcrypt from "bcrypt";
 
 export async function signUp (req, res){
+    const now = dayjs().format("YYYY-MM-DD HH:mm");
+    const user = {
+        ...res.locals.user,
+        signupdate: now
+    }
+
+    const passwordHash = bcrypt.hashSync(user.password, 10);
+
     try{
-        res.sendStatus(501);
+
+        await connection.query('INSERT INTO users (name, email, password, "signUpDate") VALUES ($1, $2, $3, $4)', [user.name, user.email, passwordHash, user.signupdate])
+        res.sendStatus(201);
     } catch(err){
         res.sendStatus(500);
         console.log(err);
