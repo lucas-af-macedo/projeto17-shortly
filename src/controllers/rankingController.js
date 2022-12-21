220c141476e3d90769eb47446c84f1e750f0d7fc
Lucas Macedo <lucasamacedo@hotmail.com>
2022-12-20 22:02:54 -0300

    feat: create ranking controller

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -2,7 +2,22 @@ import connection from "../database/database.js";
 
 export async function ranking (req, res){
     try{
-        res.sendStatus(501);
+        const query = await connection.query(`
+            SELECT 
+                users.id,
+                users.name,
+                COALESCE(COUNT (urls), 0)::INTEGER as "linksCount",
+                COALESCE(SUM (urls."visitCount"), 0)::INTEGER as "visitCount"
+            FROM 
+                users
+            LEFT JOIN urls
+            ON users.id=urls."userId"
+            GROUP BY users.id
+            ORDER BY "visitCount" DESC
+            LIMIT 10
+            `);
+        console.log(query.rows);
+        res.status(200).send(query.rows);
     } catch(err){
         res.sendStatus(500);
         console.log(err);
