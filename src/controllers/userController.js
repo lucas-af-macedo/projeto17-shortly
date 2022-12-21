d4e8dba4b80c4a6c158038f7651aecc264fbc8ed
Lucas Macedo <lucasamacedo@hotmail.com>
2022-12-20 21:35:54 -0300

    feat: create user urls controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,32 @@
 import connection from "../database/database.js";
 
 export async function userData (req, res){
+    const userId = res.locals.userId;
+    console.log(userId)
+
     try{
-        res.sendStatus(501);
+        const query = await connection.query(`
+            SELECT 
+                users.id,
+                users.name,
+                SUM (urls."visitCount")::INTEGER as "visitCount",
+                array_agg(
+                    json_build_object(
+                        'id', urls.id, 
+                        'url', urls.url, 
+                        'shortUrl', urls."shortUrl", 
+                        'visitCount', urls."visitCount") 
+                    ORDER BY urls.id) 
+                as "shortenedUrls"
+            FROM 
+                users
+            INNER JOIN urls
+            ON users.id=urls."userId"
+            WHERE users.id=$1
+            GROUP BY users.id
+            `,[userId]);
+        console.log(query.rows);
+        res.status(200).send(query.rows);
     } catch(err){
         res.sendStatus(500);
         console.log(err);
